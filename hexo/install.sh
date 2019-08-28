bd84cb4201d6ab58a61077409f813e96c4e11bdd
kcmetercec <kcmeter.cec@gmail.com>
2019-08-29 07:43:08 +0800

    <add>

---
+++ hexo/install.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+sudo npm install -g hexo-cli
+npm install hexo-deployer-git --save
+npm install hexo-generator-baidu-sitemap --save
+npm install --save hexo-generator-feed
+npm install hexo-generator-search --save
+npm install hexo-generator-searchdb --save
+npm install hexo-wordcount --save
+
